89904df394fd7aef02b7bab917b7a33aafaa6018
Matheus Ramos <msilvaramos84@gmail.com>
2022-06-07 22:28:12 -0300

    Adding hover to nav item

--- reactapp/src/components/App.js
+++ reactapp/src/components/App.js
@@ -7,9 +7,8 @@ class App extends React.Component {
     render() {
         return (
             <div className="app">
-                <HeadBar/>
+                <HeadBar />
                 <NavBar />
-                Hello World!
             </div>
         );
     }
--- reactapp/src/components/NavItem.js
+++ reactapp/src/components/NavItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './NavItem.css';
 
 class NavItem extends React.Component {
     render() {
