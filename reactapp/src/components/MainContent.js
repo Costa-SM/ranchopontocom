17e8dbc146663633fa640446114c3165c6ce1a45
edmsimp <63891357+edmsimp@users.noreply.github.com>
2022-06-14 18:08:35 -0300

    add: week item image

--- reactapp/src/components/MainContent.js
+++ reactapp/src/components/MainContent.js
@@ -8,8 +8,8 @@ class MainContent extends React.Component {
         return (
             <div className="main_content">
                 <Fade clear>
-                    <WeekItem weekDay="SEGUNDA" monthDay="6 JUN"/>
-                    <WeekItem weekDay="TERÇA" monthDay="7 JUN"/>
+                    <WeekItem weekDay="SEGUNDA" monthDay="6 JUN" url="https://upload.wikimedia.org/wikipedia/commons/6/6b/Moqueca.jpg"/>
+                    <WeekItem weekDay="TERÇA" monthDay="7 JUN" url="http://s2.glbimg.com/gl5oRhx58Ze3TBxM5hCRfT2M9us=/780x440/e.glbimg.com/og/ed/f/original/2017/06/19/feijao-porco.jpg"/>
                     <WeekItem weekDay="QUARTA" monthDay="8 JUN"/>
                     <WeekItem weekDay="QUINTA" monthDay="9 JUN"/>
                     <WeekItem weekDay="SEXTA" monthDay="10 JUN"/>
--- reactapp/src/components/WeekItem.js
+++ reactapp/src/components/WeekItem.js
@@ -1,17 +1,43 @@
 import React from 'react';
+import styled from 'styled-components';
 import './WeekItem.css'
 
 class WeekItem extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
+
+        const Item = styled.div`
+        /* background-color is value of props.color */
+        background-color: white;
+        color: white;
+        font-weight: 500;
+        padding: 10px;
+        margin: auto;
+        margin-bottom: 20px;
+        margin-top: 20px;
+        border-radius: 8px;
+        box-shadow: 0 2px 5px rgb(0 0 0 / 15%);
+        width: 65vw;
+        max-width: 880px;
+        height: 100px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        background: url(${props => props.url}) center;
+        background-size: cover;
+    `;
         return (
-            <div className="week_item">
+            <Item url={this.props.url}>
                 <div className="week_day">
                     {this.props.weekDay}
                 </div>
                 <div className="month_day">
                     {this.props.monthDay}
                 </div>
-            </div>
+            </Item>
         );
     }
 }
