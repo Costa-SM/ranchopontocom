1da1c73149b95445f4c537e7d1823437b2583a61
Matheus Ramos <msilvaramos84@gmail.com>
2022-06-21 16:47:22 -0300

    Implementing fetch

--- reactapp/src/components/SignBox.js
+++ reactapp/src/components/SignBox.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import axios from 'axios';
 import './SignBox.css';
+import bcrypt from 'bcrypt';
 
 class SignBox extends React.Component {    
     constructor(props) {
@@ -7,31 +9,80 @@ class SignBox extends React.Component {
         this.state = { email: '', password: '', text: ''};
     }
 
-    onFormSubmit = async (event) => {
-        event.preventDefault();
-
-        const response = await fetch('http://127.0.0.1:8000/core');
-        const json = await response.json();
-
-        if (this.state.email === json.email && this.state.password === json.password) {
-            this.setState({ text: 'You are logged in' });
+    validate = async (email, password, json) => {
+        for (let i = 0; i < json.length; i++) {
+            if (email === json[i].email) {
+                await bcrypt.compare(password, json[i].password, function(err, result) {
+                    if (err) {
+                        throw Error('Unable to compare');
+                    }
+                    return result;
+                });
+            }
         }
-        else {
-            this.setState({ text: 'Unable to login' });
+        return false;
+    }
+
+    fetchData = async () => {
+        try {
+            const response = await fetch('http://127.0.0.1:8000/api/users');
+            const json = await response.json();
+            
+            const validCredentials = this.validate(this.state.email, this.state.password, json);
+
+            if (validCredentials) {
+                this.setState({ text: 'You are logged in' });
+            }
+            else {
+                this.setState({ text: 'Unable to login' });
+            }
         }
+        catch(e) {
+            console.log(e);
+        }
+    }
+
+    postData = async () => {
+        let password;
+
+        await bcrypt.genSalt(10, function(err, salt) {
+            bcrypt.hash(this.state.password, salt, function(err, hash) {
+                if (err) {
+                    throw Error('Unable to hash');
+                }
+                password = hash;
+            });
+        });
 
         const myData = {
             email: this.state.email,
-            password: this.state.password
+            password
         };
+
+        try {
+            await axios.post('http://127.0.0.1:8000/api/users/', myData);
+        } catch(e) {
+            console.log(e);
+        }
+    }
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+
+        if (this.props.mainText === 'Entrar') {
+            this.fetchData();
+        }
+        else {
+            this.postData();
+        }
       
-        await fetch('http://127.0.0.1:8000/core/', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(myData)
-        });
+        // await fetch('http://127.0.0.1:8000/core/', {
+        //     method: 'POST',
+        //     headers: {
+        //       'Content-Type': 'application/json'
+        //     },
+        //     body: JSON.stringify(myData)
+        // });
     }
 
     render() {
