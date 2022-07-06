9dc253043ac09d951df696615a4868266debe681
edmsimp <63891357+edmsimp@users.noreply.github.com>
2022-07-05 21:23:55 -0300

    test

--- ranchopontocom/settings.py
+++ ranchopontocom/settings.py
@@ -137,9 +137,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-
-
-
 import django_heroku
 
 django_heroku.settings(locals())
