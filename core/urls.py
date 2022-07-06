13fad0356cf9e2ce9fb2027da320f965b9dbb2ec
edmsimp <63891357+edmsimp@users.noreply.github.com>
2022-07-06 15:10:30 -0300

    fix urls

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,29 @@
-from django.urls import path
+"""ranchopontocom URL Configuration
 
-from . import views
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from core import views
+
+router = routers.DefaultRouter()
+router.register('users', views.UserView, 'user')
+router.register('menus', views.MenusView, 'menus')
+router.register('feedbacks', views.FeedbackView, 'feedback')
 
 urlpatterns = [
-    path('', views.page, name='page'),
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls))
 ]
